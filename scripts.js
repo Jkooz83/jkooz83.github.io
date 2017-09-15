a0e9d33f96d0c071c2398b47e772626961cda013
Jkooz83 <31411807+Jkooz83@users.noreply.github.com>
2017-09-15 07:01:10 -0700

    Week 3 IT2320
    Addition of JS file

---
+++ scripts.js
@@ -0,0 +1,34 @@
+﻿function MouseOverListItem(li)
+
+{
+
+	li.className = "list-item-highlight";
+
+}
+
+function MouseOutListItem(li)
+
+{
+
+	li.className = "";
+
+}
+
+function myFunction() {
+    var mtjWins, wins;
+    mtjWins = document.getElementById("mtjWins").value;
+    wins = (mtjWins > 4) ? "Wow! You saw his success coming this year!":"You thought wrong, but then again no one saw his success coming!";
+    document.getElementById("demo").innerHTML = wins;
+}
+
+
+function myFunctionforTime()
+{
+document.getElementById("time").innerHTML = Date();
+
+}
+
+function button()
+{
+document.getElementById("raceWinner").innerHTML="Kevin Harvick!">;
+}
