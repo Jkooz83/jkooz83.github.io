4a9257b7d6675d490162347fb985e90c5010206b
Jkooz83 <31411807+Jkooz83@users.noreply.github.com>
2017-09-18 12:34:35 -0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@
 
 }
 
+
+
 function MouseOutListItem(li)
 
 {
@@ -15,20 +17,31 @@ function MouseOutListItem(li)
 }
 
 function myFunction() {
+
     var mtjWins, wins;
+
     mtjWins = document.getElementById("mtjWins").value;
+
     wins = (mtjWins > 4) ? "Wow! You saw his success coming this year!":"You thought wrong, but then again no one saw his success coming!";
+
     document.getElementById("demo").innerHTML = wins;
+
 }
 
+function myfunctionforWinner()
+
+{
+
+document.getElementById("raceWinner").innerHTML="Kevin Harvick!";
+
+}
 
 function myFunctionforTime()
+
 {
+
 document.getElementById("time").innerHTML = Date();
 
-}
 
-function button()
-{
-document.getElementById("raceWinner").innerHTML="Kevin Harvick!">;
+
 }
