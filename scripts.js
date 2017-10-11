335fbb71751737ff671b6af60cfa6aaea7e6f956
Jkooz83 <31411807+Jkooz83@users.noreply.github.com>
2017-10-11 16:39:45 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,47 +1,139 @@
-﻿function MouseOverListItem(li)
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
+function displayTeam(team)
 {
+	alert("The following drivers race for Team" + " " + team.make);
+	alert("They drive a 2017" + " " + team.model);
 
-	li.className = "list-item-highlight";
-
+	for (var i=0; i<team.lineup.length; i++)
+	{
+		var player = team.lineup[i];
+		alert(player.driver+ " " +player.carNumber);
+	}
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-function MouseOutListItem(li)
-
+function createTeamAsCHEVY()
 {
+	return {
 
-	li.className = "";
+		"make" : "Chevrolet",
+		"model" : "Camaro",
+		"lineup" : [
 
+			{
+				"carNumber" : "88",
+				"driver" : "Dale Earnhardt Jr",			
+				},
+				
+			{	
+				"carNumber" : "42",
+				"driver" : "Kyle Larson",
+				"carOwner" : "Chip Gannassi"
+			},
+
+			{
+				"carNumber" : "27",
+				"driver" : "Paul Menard",
+				}
+			
+		]
+
+	};
 }
 
-function myFunction() {
-
-    var mtjWins, wins;
-
-    mtjWins = document.getElementById("mtjWins").value;
-
-    wins = (mtjWins > 4) ? "Wow! You saw his success coming this year!":"You thought wrong, but then again no one saw his success coming!";
-
-    document.getElementById("demo").innerHTML = wins;
-
-}
-
-function myfunctionforWinner()
-
+function createTeamAsFORD()
 {
+	return {
 
-document.getElementById("raceWinner").innerHTML="Kevin Harvick!";
+		"make" : "Ford",
+		"model" : "Mustang",
+		"lineup" : [
 
+			{
+				"carNumber" : "4",
+				"driver" : "Kevin Harvick"
+			},
+
+			{
+	            "carNumber" : "14",
+				"driver" : "Clint Bowyer"
+			},
+
+			{
+				"carNumber" : "2",
+				"driver" : "Brad Keselowski"
+
+			}
+
+		]
+
+	};
 }
 
-function myFunctionforTime()
-
+function createTeamAsTOYOTA()
 {
+	return {
 
-document.getElementById("time").innerHTML = Date();
+		"make" : "Toyota",
+		"model" : "Camry",
+		"lineup" : [
 
+			{
+				"carNumber" : "11",
+				"driver" : "Denny Hamlin"
+			},
 
+			{
+	            "carNumber" : "18",
+				"driver" : "Kyle Busch"
+			},
 
+			{
+				"carNumber" : "19",
+				"driver" : "Daniel Suarez"
+
+			}
+
+		]
+
+	};
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function()
+{
+	$(".button").mouseover(function()
+	{
+		$(this).css("border", "6px solid red");
+	});
+
+	$(".button").mouseout(function()
+	{
+		$(this).css("border", "6px solid blue");
+	});
+
+	$(".button1").click(function()
+	{
+		var team = createTeamAsCHEVY();
+		displayTeam(team);
+	});
+	
+	$(".button2").click(function()
+	{
+		var team = createTeamAsFORD();
+		displayTeam(team);
+	});
+	
+	$(".button3").click(function()
+	{
+		var team = createTeamAsTOYOTA();
+		displayTeam(team);
+		});
+
+});
