d258dc5a605b169cf33a73c89aab61e8e3cbbdb1
Jkooz83 <31411807+Jkooz83@users.noreply.github.com>
2017-10-16 15:19:45 -0400

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,69 @@
+﻿var isHoldingPiece = false;
+var pieceInHand = {};
+
+PickUpPiece = function (element) {
+    $(element).css
+
+        (
+        "border", "4px solid green"
+        );
+    pieceInHand = element;
+    isHoldingPiece = true;
+}
+
+
+PutPieceInHandOnElement = function (targetElement) {
+    targetElement.className = pieceInHand.className;
+    $(pieceInHand).css
+
+        (
+        "border", "2px solid black"
+        );
+
+    pieceInHand.className = "cell";
+    isHoldingPiece = false;
+    pieceInHand = {};
+
+}
+
+
+$(document).ready(function () {
+
+    var cells = $(".cell");
+
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++) {
+
+        var cell = $(cells[i]);
+
+        var isDark = colorCount % 2 == 0;
+
+        var isNextRow = (i + 1) % 8 == 0;
+
+        colorCount += isNextRow ? 2 : 1;
+
+        cell.css("background-color", isDark ? "navy" : "white");
+
+    }
+
+    $(".cell").click(function () {
+
+        if (!isHoldingPiece) {
+
+            if ($(this).hasClass("piece")) {
+
+                PickUpPiece(this)
+
+            }
+
+        }
+
+        else {
+
+            PutPieceInHandOnElement(this)
+
+        }
+    });
+});
+
