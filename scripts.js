71fb0ffc4fec97bee2eff0599413838a92b6cf0d
Jkooz83 <31411807+Jkooz83@users.noreply.github.com>
2017-10-23 14:50:27 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,47 @@
+﻿function MouseOverListItem(li)
+
+{
+
+	li.className = "list-item-highlight";
+
+}
+
+
+
+function MouseOutListItem(li)
+
+{
+
+	li.className = "";
+
+}
+
+function myFunction() {
+
+    var mtjWins, wins;
+
+    mtjWins = document.getElementById("mtjWins").value;
+
+    wins = (mtjWins > 4) ? "Wow! You saw his success coming this year!":"You thought wrong, but then again no one saw his success coming!";
+
+    document.getElementById("demo").innerHTML = wins;
+
+}
+
+function myfunctionforWinner()
+
+{
+
+document.getElementById("raceWinner").innerHTML="Kevin Harvick!";
+
+}
+
+function myFunctionforTime()
+
+{
+
+document.getElementById("time").innerHTML = Date();
+
+
+
+}
