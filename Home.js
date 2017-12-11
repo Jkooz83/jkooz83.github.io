be81b0ecd224da67a224df1607ef4b72e48395a1
Jkooz83 <31411807+Jkooz83@users.noreply.github.com>
2017-12-11 14:17:41 -0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,52 @@
+﻿
+function contentAnimation() {
+
+    $('#login').click(function () {
+        $(".login").hide('200');
+        $(".account").show('500');
+
+    })
+
+    $('#create').click(function () {
+        $(".login").hide('200');
+        $(".account").show('500');
+    })
+
+}
+
+$(document).ready(function () {
+    $('.account').hide();
+
+
+    contentAnimation();
+
+    $('.login').click(function () {
+
+        $.ajax({
+
+            data: {
+                "Username": $(".userName").val(),
+            }
+
+
+        })
+    })
+
+    $('.create').click(function () {
+
+        $.ajax({
+
+
+            data: {
+                "Username": $(".newUN").val(),
+                "Password": $(".newPW").val(),
+                "EmailAdd": $(".emailAddress").val(),
+            },
+
+            success: function (createAccount) {
+                alert(Message);
+            }
+        })
+    })
+});
+
